30a998381f11a14c3869eff3bc784feb566000e5
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-19 15:17:03 +0200

    seeder funzionante

--- database/seeds/TravelPackagesTableSeeder.php
+++ database/seeds/TravelPackagesTableSeeder.php
@@ -11,8 +11,19 @@ class TravelPackagesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+      for ($i=0; $i < 10; $i++) {
+        $newObject = new Travel_package();
+        $newObject->location = $faker->city();
+        $newObject->location_type = $faker->randomElement(['Mare', 'Montagna', 'SPA', 'Città d\'arte']);
+        $newObject->accomodation = $faker->company();
+        $newObject->accomodation_type = $faker->randomElement(['Hotel', 'Ostello', 'B&B', 'Appartamento', 'Camping']);
+        $newObject->nights = $faker->numberBetween(1, 10);
+        $newObject->transfer_included = $faker->numberBetween(0, 1);
+        $newObject->peolpe_maximum = $faker->numberBetween(1, 5);
+        $newObject->cost_per_person = $faker->randomFloat(2, 100, 250);
+        $newObject->save();
+      }
     }
 }
