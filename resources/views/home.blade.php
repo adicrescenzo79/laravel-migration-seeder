42a441509732a434900ea32323b9bc79886170aa
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-19 19:56:59 +0200

    style main completo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,23 +3,32 @@
 
 
 <main id="home">
-  <div class="container">
-    <div class="flip-card">
-      <div class="flip-card-inner">
-        <div class="flip-card-front">
-          <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
-        </div>
-        <div class="flip-card-back">
-          <h1>John Doe</h1>
-          <p>Architect & Engineer</p>
-          <p>We love that guy</p>
+  <div class="container flex">
+    @foreach ($travelPackages as $key => $pack)
+
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="https://picsum.photos/200/300?random={{$key}}" alt="Avatar">
+            <div class="text flex">
+              <p>{{$pack->location_type}}!!! {{$pack->nights}} notti a</p>
+              <h1>{{$pack->location}}</h1>
+              <p>a soli {{$pack->cost_per_person}}€ a persona</p>
+            </div>
+          </div>
+          <div class="flip-card-back flex">
+            <p>{{$pack->accomodation_type}}</p>
+            <h1>{{$pack->accomodation}}</h1>
+            <p>Viaggio {{($pack->transfer_included) ? '' : 'non'}} incluso</p>
+            <p>Massimo {{$pack->peolpe_maximum}} persone</p>
+          </div>
         </div>
       </div>
-    </div>
+
+    @endforeach
+
   </div>
 </main>
-
-
 
 
 
